ef82cd86f7fca8799a28b4e953c5cb97252a0f3d
Changmin Yeo <minichangmin@dgist.ac.kr>
2024-08-25 23:06:20 +0900

    first commit

---
+++ CMakeFiles/simulation.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulation.dir/src/controller/TrackingController.cpp.o"
+  "CMakeFiles/simulation.dir/src/glfw_adapter.cc.o"
+  "CMakeFiles/simulation.dir/src/glfw_dispatch.cc.o"
+  "CMakeFiles/simulation.dir/src/lodepng.cpp.o"
+  "CMakeFiles/simulation.dir/src/main.cc.o"
+  "CMakeFiles/simulation.dir/src/platform_ui_adapter.cc.o"
+  "CMakeFiles/simulation.dir/src/robot/MuJoCoInterface.cpp.o"
+  "CMakeFiles/simulation.dir/src/robot/RobotLeg.cpp.o"
+  "CMakeFiles/simulation.dir/src/simulate.cc.o"
+  "CMakeFiles/simulation.dir/src/trajectory/MotionTrajectory.cpp.o"
+  "CMakeFiles/simulation.dir/src/utilities/data_logging.cpp.o"
+  "simulation"
+  "simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
